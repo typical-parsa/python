0dc22eed0ca4f6df391d00ec74b12c50f4089d2e
typicalparsa <typicalparsa@gmail.com>
2025-11-24 23:58:55 +0330

    python tkinter label

--- tkinter/main.py
+++ tkinter/main.py
@@ -4,4 +4,8 @@ root = tk.Tk()
 root.title("Test Window")
 root.geometry("800x600")
 root.resizable(False, False)
+my_label = tk.Label(root, text = "Hello world" )
+my_label.pack()
+
+
 root.mainloop()
