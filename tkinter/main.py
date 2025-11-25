bfc637a095bc4bbb7b93081b40e7cc646eb53c26
typicalparsa <typicalparsa@gmail.com>
2025-11-25 15:53:39 +0330

    python tkinter show/hide widget

--- tkinter/main.py
+++ tkinter/main.py
@@ -10,6 +10,9 @@ my_label2 = tk.Label(root, text="Not clicked yet", fg="red")
 my_label2.pack()
 my_label3 = tk.Label(root, text="", fg="black")
 my_label3.pack()
+global my_label4
+my_label4 = tk.Label(root, text="test text", fg="black")
+my_label4.pack()
 entry_box = tk.Entry(root, width=300)
 entry_box.pack()
 
@@ -19,9 +22,18 @@ def clicked():
     input_text = entry_box.get()
     my_label3.config(text=input_text, fg="black")
 
+def hide():
+    my_label4.pack_forget()
+
+def show():
+    my_label4.pack()
 
 my_button = tk.Button(root, text="click me", command= clicked )
 my_button.pack()
+hide_button = tk.Button(root, text="Hide", command=hide)
+hide_button.pack()
+show_button = tk.Button(root, text="Show", command=show)
+show_button.pack()
 
 
 
