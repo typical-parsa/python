c37239bced281e07a57ee78bfd316a1afda9bef6
typicalparsa <typicalparsa@gmail.com>
2025-11-25 15:38:38 +0330

    python tkinter entry box

--- tkinter/main.py
+++ tkinter/main.py
@@ -6,7 +6,17 @@ root.geometry("800x600")
 root.resizable(False, False)
 my_label = tk.Label(root, text = "Hello world", fg="blue" )
 my_label.grid(row=0, column=0)
-my_button = tk.Button(root, text="clieck me" )
+my_label2 = tk.Label(root, text="Not clicked yet", fg="red")
+my_label2.grid(row=3, column=3)
+
+def clicked():
+    my_label2.config(text="you've clicked the button", fg="green")
+    my_label.config(text="By by world", fg="pink")
+
+
+my_button = tk.Button(root, text="click me", command= clicked )
 my_button.grid(row=4, column=5)
 
+entry_box = tk.Entry(root, width=300)
+entry_box.grid(row=20, column=20)
 root.mainloop()
