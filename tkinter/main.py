eee445148034c33e666f7993cde2e9b75b11e3ee
typicalparsa <typicalparsa@gmail.com>
2025-11-25 15:43:13 +0330

    python tkinter getting info from entry box

--- tkinter/main.py
+++ tkinter/main.py
@@ -5,18 +5,24 @@ root.title("Test Window")
 root.geometry("800x600")
 root.resizable(False, False)
 my_label = tk.Label(root, text = "Hello world", fg="blue" )
-my_label.grid(row=0, column=0)
+my_label.pack()
 my_label2 = tk.Label(root, text="Not clicked yet", fg="red")
-my_label2.grid(row=3, column=3)
+my_label2.pack()
+my_label3 = tk.Label(root, text="", fg="black")
+my_label3.pack()
+entry_box = tk.Entry(root, width=300)
+entry_box.pack()
 
 def clicked():
     my_label2.config(text="you've clicked the button", fg="green")
-    my_label.config(text="By by world", fg="pink")
+    my_label.config(text="By by world", fg="purple")
+    input_text = entry_box.get()
+    my_label3.config(text=input_text, fg="black")
 
 
 my_button = tk.Button(root, text="click me", command= clicked )
-my_button.grid(row=4, column=5)
+my_button.pack()
 
-entry_box = tk.Entry(root, width=300)
-entry_box.grid(row=20, column=20)
+
+
 root.mainloop()
