fbc0c06f924ce12bfbdad7022827c3aa64dcbd26
typicalparsa <typicalparsa@gmail.com>
2025-11-30 16:09:39 +0330

    python django dynamic url parameter

--- django-practice/mysite/home/views.py
+++ django-practice/mysite/home/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'home/index.html')
+
+def usershow(request, username):
+    return HttpResponse(f'hello {username}')
