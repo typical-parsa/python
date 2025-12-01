d7af538283c774a86d39a184110d3eb963a8c88e
typicalparsa <typicalparsa@gmail.com>
2025-12-01 11:50:02 +0330

    python cleanup

--- django-practice/mysite/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("adduser", views.user_signup_page)
-]
--- django-practice/mysite/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('users/<username>', views.usershow),
-    path('messages', views.message_show)
-]
