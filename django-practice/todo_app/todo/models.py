41208805320f98287da3ae87a4d00a9f98dc890f
typicalparsa <typicalparsa@gmail.com>
2025-12-02 12:24:34 +0330

    python django registring Todo model

--- django-practice/todo_app/todo/models.py
+++ django-practice/todo_app/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Todo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField()
+    description = models.TextField()
+    category = models.CharField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title}'
