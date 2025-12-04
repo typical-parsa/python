d931d82e3331f9ea17174ee233874339c43b7ab1
typicalparsa <typicalparsa@gmail.com>
2025-12-04 22:43:40 +0330

    python cleanup

--- django-practice/todo_app/todo/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('todos', views.todo_list, name='todo-list'),
-    path('create/', views.create_todo, name='create-todo')
-]
